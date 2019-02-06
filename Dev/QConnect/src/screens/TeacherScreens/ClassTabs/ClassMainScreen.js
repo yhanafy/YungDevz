17ccb15532a81065cd789219eb38d65e650d2fd8
Elyasse El Yacoubi <elyassedev@outlook.com>
2019-02-06 15:27:54 -0800

    Hooked up some mock up students and added a model folder structure for future data hookup.

--- Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassMainScreen.js
+++ Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassMainScreen.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import {ListItem} from 'react-native-elements'
 
 class ClassMainScreen extends React.Component {
-    render() {
+    state = {
+        students: []
+    };
+
+    componentDidMount() {
+        const students = require('model/class.json').students.map(s => ({
+                ...s
+        }));
+        this.setState( {students} );
+    }
+
+    render() {        
         return (
-            <View><Text style={{marginTop: 50}}>السلام عليكم أستاذ</Text>
-            <Text>Class Main Screen</Text></View>
-        );
+            <View>
+                {this.state.students.map((u, i) => {
+              return (
+                <ListItem
+                    key={i}
+                    roundAvatar
+                    title={u.name}
+                    leftAvatar={{ rounded: true, source: { uri: u.avatar } }}
+                    subtitle={u.assignment}
+                    />
+              );
+            })}
+            </View>);
     }
 }
 
