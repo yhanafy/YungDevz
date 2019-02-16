e665a5d429679f51621c86422a218735d9538790
awsm36z <awsmdud@gmail.com>
2019-02-16 14:02:26 -0800

    Change edit class  and add class screens

--- Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassEditScreen.js
+++ Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassEditScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Image} from 'react-native';
 
 class ClassEditScreen extends React.Component {
     render() {
         return (
-            <View><Text  style={{marginTop: 50}}>Placeholder for Class Update screen...</Text></View>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}><Image style={ {width: 200, height: 200, backgroundColor: "blue"} } source= {{uri: 'https://www.whyislam.org/wp-content/uploads/2017/10/Glorious-Quran-.jpg'}}></Image></View>
         );
     }
 }
