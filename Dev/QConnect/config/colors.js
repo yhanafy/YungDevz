545e10e84c8050951eeb943c033cca258ad8d656
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-02-20 16:01:46 -0800

    Update colors.js

--- Dev/QConnect/config/colors.js
+++ Dev/QConnect/config/colors.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default {
   black: '#101011',
   white: '#fdfdff',
@@ -12,18 +11,3 @@ export default {
   darkGrey: '#6b6b6b',
   darkestGrey: '#494949',
 };
-=======
-export default {
-  black: '#101011',
-  white: '#fdfdff',
-  primaryLight: '#efdecc',
-  primaryDark: '#937f6b',
-  green: '#CEEFCC',
-  red: '#F7D3D3',
-  veryLightGrey: '#f2f2f2',
-  lightGrey: '#F3F3F3',
-  grey: '#dadada',
-  darkGrey: '#6b6b6b',
-  darkestGrey: '#494949',
-};
->>>>>>> ea44763c39dea33f9c99099b1a1f473c3e04582e
