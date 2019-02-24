14a0efbf576c844490ca741be09c218c6be60dac
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-02-24 10:36:14 -0800

    Added mock attendence history for each student

--- Dev/QConnect/src/model/reducers/classReducer.js
+++ Dev/QConnect/src/model/reducers/classReducer.js
@@ -8,35 +8,71 @@ const INITIAL_STATE = {
           {
             name: "Ahmed Reducer",
             avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
-            assignment: "Al-Nahl page 5"
+            assignment: "Al-Nahl page 5",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: true
+              }
+            ]
           },
           {
             name: "Amina Khan",
             avatar: "https://randomuser.me/api/portraits/thumb/women/42.jpg",
-            assignment: "An-Naze'aat"
+            assignment: "An-Naze'aat",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: true
+              }
+            ]
           },
           {
             name: "Ayoub Barrak",
             avatar: "https://randomuser.me/api/portraits/thumb/men/43.jpg",
-            assignment: "Aal-Imran"
+            assignment: "Aal-Imran",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: false
+              }
+            ]
           },
           {
             name: "Khaled Kwick",
             avatar: "https://randomuser.me/api/portraits/thumb/men/45.jpg",
-            assignment: "Al-Toor pages 2,3"
+            assignment: "Al-Toor pages 2,3",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: true
+              }
+            ]
           },
           {
             name: "Yassine Lightening",
             avatar: "https://randomuser.me/api/portraits/thumb/men/22.jpg",
-            assignment: "Al-An'aam"
+            assignment: "Al-An'aam",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: true
+              }
+            ]
           },
           {
             name: "Yusuf Awesome",
             avatar: "https://randomuser.me/api/portraits/thumb/men/26.jpg",
-            assignment: "Huud, pages 3 and 4"
+            assignment: "Huud, pages 3 and 4",
+            attendenceHistory: [
+              {
+                date: "02-23-2019",
+                isHere: false
+              }
+            ]
           }
-        ]
-      }
+        ],
+    }
 
   ]
 
