69312264c405ec221a28a0bd7be6e555d54c40d3
Elyasse <elyassedev@outlook.com>
2019-02-27 20:42:03 -0800

    align with store name in master

--- Dev/QConnect/App.js
+++ Dev/QConnect/App.js
@@ -10,7 +10,7 @@ import { AsyncStorage } from 'react-native';
 
 
 const persistConfig = {
-  key: 'qconnectalpha',
+  key: 'qcstorealpha',
   storage: AsyncStorage
 }
 const persistedReducer = persistReducer(persistConfig, classReducer)
