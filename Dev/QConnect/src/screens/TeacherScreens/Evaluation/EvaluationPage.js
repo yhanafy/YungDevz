eebd2d1f9f77f87b2631c74588f4e851c3b9898a
yhanafy <45704361+yhanafy@users.noreply.github.com>
2019-03-03 20:22:09 -0800

    Update EvaluationPage.js

--- Dev/QConnect/src/screens/TeacherScreens/Evaluation/EvaluationPage.js
+++ Dev/QConnect/src/screens/TeacherScreens/Evaluation/EvaluationPage.js
@@ -1,61 +1,56 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput, Image, Platform, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import { connect } from 'react-redux';
+import StudentCard from 'components/StudentCard'
+import colors from 'config/colors'
+import newStudentName from 'ClassEditScreen'
+import { withBadge, Header } from 'react-native-elements';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import { connect } from 'react-redux';
+import StudentCard from 'components/StudentCard'
 import colors from 'config/colors'
 
+//This page is for the teacher to evaluate the students after they make memorazation. This page will consist  
+class EvaluationPage extends Component {
+    render(){
+        return(
+            <StudentCard
+            key={i}
+            studentname={studentname}
+            profilePic={student.avatar}
+            currentAssignment={student.assignment}
+            />
+        );
+    
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'colomn',
+        backgroundColor: colors.lightGrey,
+        flex: 1
+    },
+    
+})
 
-class AddClassScreen extends Component {
+//This is the header that will say Student Evalutation in the middle of the box      
 
-  state = {
-    className: '',
-  }
-  render() {
-    const { navigate } = this.props.navigation;
+//right under the Header of this page there will be the students profile pic/icon.
 
-    return (
+//Under the pic will be the name of the student that the teacher is evaluating
 
-      <View ID="addNewClass" style={{
-        alignItems: "center",
-        justifyContent: "center",
-      }}>
+//UNder the name it is the students previous assignment that they just completed right then
 
+//Under the assignment is going to be the How was ____Name____ tasmee' today only words
 
-        <TextInput
-          style={{
+//under that there will be the five star rating for that person
 
-            backgroundColor: colors.lightGrey,
-            borderColor: colors.darkGrey,
-            width: 250,
-            textAlign: "center",
-            alignItems: "center",
-            justifyContent: "center"
+//Under that there will be a {"Give a badge?"}
 
-          }}
+//Under that there is there four badges that cann all be clicked on to give to the student.
 
-          placeholder='Write Class Name Here'
+//under that there is text box that can be written and inside that text box says "write a note"
 
-          onChangeText={(classInput) => this.setState({
+//At the very bottom in the footer there is a long ellipse button that is baige that will say Submit
 
-            className: classInput,
-
-          })}
-
-
-        />
-        <Image source={{ uri: '<iframe width="901" height="507" src="https://www.youtube.com/embed/2TQWbxxyJUA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' }}
-          style={{
-            width: 200,
-            height: 200,
-            alignItems: 'center',
-            borderRadius: 150 / 2
-          }} />
-
-        <Text>Your Class name is {this.state.className}</Text>
-      </View>
-    );
-  }
-
-
-
-};
-
-export default AddClassScreen;
+//Right next to the submit button there is a text button which says Skip> 
