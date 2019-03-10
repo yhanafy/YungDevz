718768ae9f24c6999d3365d16d50d1c7b61cabbf
awsm36z <awsmdud@gmail.com>
2019-03-10 15:27:07 -0700

    Update AddClassScreen.js

--- Dev/QConnect/src/screens/TeacherScreens/AddClass/AddClassScreen.js
+++ Dev/QConnect/src/screens/TeacherScreens/AddClass/AddClassScreen.js
@@ -127,7 +127,7 @@ export class AddClassScreen extends Component {
         <QcActionButton
           text="Add Class"
           onPress={() => {
-            this.setModalVisible(true)
+            this.addNewClass();
           }}
         />
       </View>
