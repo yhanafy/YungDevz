beb2ee8591fab894ebb4d8e4e45773acb5e20175
Elyasse <elyassedev@outlook.com>
2019-03-10 20:18:58 -0700

    Improve readibilty and code reuse of reducers tests

--- Dev/QConnect/__tests__/redux/TeacherReducers.js
+++ Dev/QConnect/__tests__/redux/TeacherReducers.js
@@ -2,6 +2,61 @@ import { classReducer, INITIAL_STATE } from 'model/reducers/classReducer';
 
 describe('teacher reducer', () => {
 
+    //------ test consts --------
+    const teacher_props = {
+        name: "Teacher Name 1",
+        phoneNumber: "123-456-789",
+        emailAddress: "test@email.com",
+        currentClassIndex: 0,
+    };
+
+    const teacher_no_class = {
+        teachers: [
+            {
+                ...teacher_props,
+                classes: []
+            }]
+    };
+
+    const classInfo = {
+        name: "test class",
+        imageId: 1,
+        students: []
+    };
+
+    const teacher_one_class_no_students = {
+        teachers: [
+            {
+                ...teacher_props,
+                classes: [
+                    classInfo
+                ]
+            }
+        ]
+    };
+
+    const studentInfo = {
+        name: "Test Student 1",
+        avatar: "http://test.avatar.url",
+        assignment: "Test assignment",
+        attendanceHistory: []
+    };
+
+    const teacher_one_class_one_students = {
+        teachers: [
+            {
+                ...teacher_props,
+                classes: [
+                    {
+                        ...classInfo,
+                        students: [
+                            studentInfo
+                        ]
+                    }
+                ]
+            }
+        ]
+    };
 
     //------------ Initial state test ---------------------
     it('should return the initial state', () => {
@@ -10,101 +65,26 @@ describe('teacher reducer', () => {
 
     //------------ AddClass test ---------------------
     it('should handle ADD_CLASS', () => {
-        const teacher_props = {
-            name: "Teacher Name 1",
-            phoneNumber: "123-456-789",
-            emailAddress: "test@email.com",
-            currentClassIndex: 0,
-        };
-
-        const initial_state = {
-            teachers: [
-                {
-                    ...teacher_props,
-                    classes: []
-                }]
-        };
-
-        const classInfo = {
-            name: "test class",
-            imageId: 1,
-            students: []
-        };
-
         expect(
-            classReducer(initial_state, {
+            classReducer(teacher_no_class, {
                 type: "ADD_CLASS",
                 classInfo
             })
-        ).toEqual({
-            teachers: [
-                {
-                    ...teacher_props,
-                    classes: [
-                        classInfo
-                    ]
-                }
-            ]
-        });
+        ).toEqual(teacher_one_class_no_students);
     })
 
     //------------ AddStudent test ---------------------
     it('should handle ADD_STUDENT', () => {
-        const initial_state = {
-            teachers: [
-                {
-                    name: "Teacher Name 1",
-                    phoneNumber: "123-456-789",
-                    emailAddress: "test@email.com",
-                    currentClassIndex: 0,
-                    classes: [{
-                        name: "test class",
-                        imageId: 1,
-                        students: []
-                    }
-                    ]
-                }]
-        };
-
-        const studentInfo = {
+        let payload = {
             classIndex: 0,
-            studentInfo: {
-                name: "Test Student 1",
-                avatar: "http://test.avatar.url",
-                assignment: "Test assignment",
-                attendanceHistory: []
-            }
-        };
-
+            studentInfo: studentInfo
+        }
         expect(
-            classReducer(initial_state, {
+            classReducer(teacher_one_class_no_students, {
                 type: "ADD_STUDENT",
-                studentInfo
+                studentInfo: payload
             })
-        ).toEqual({
-            teachers: [
-                {
-                    name: "Teacher Name 1",
-                    phoneNumber: "123-456-789",
-                    emailAddress: "test@email.com",
-                    currentClassIndex: 0,
-                    classes: [
-                        {
-                            name: "test class",
-                            imageId: 1,
-                            students: [
-                                {
-                                    name: "Test Student 1",
-                                    avatar: "http://test.avatar.url",
-                                    assignment: "Test assignment",
-                                    attendanceHistory: []
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        });
+        ).toEqual(teacher_one_class_one_students);
     })
 
 })
