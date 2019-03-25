b2fa545a3f5f6359c61e71a8a9c3eb4d1f8ae5d1
Elyasse <elyassedev@outlook.com>
2019-03-26 00:26:47 +0100

    Evaluation Page: hook to redux + start some initial styling

--- Dev/QConnect/src/screens/Evaluation/EvaluationPage.js
+++ Dev/QConnect/src/screens/Evaluation/EvaluationPage.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
 import { Rating, AirbnbRating } from 'react-native-elements';
 import colors from 'config/colors';
+import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
 
 class EvaluationPage extends Component {
 
@@ -9,24 +11,54 @@ class EvaluationPage extends Component {
     return (
 
       <View
-        style={{
-          alignContent: "center",
-          
-          justifyContent: "center",
-        }}>
-        <Rating
-          showRating
-          onFinishRating={this.ratingCompleted}
-          style={{
-            paddingVertical: 10,
-            backgroundColor: colors.lightGrey,
-            
-          }}
-        />
+        style={styles.container}>
+        <View
+          style={styles.evaluationContainer}>
+          <Text>Student Evaluation: {this.props.name}</Text>
+          <Rating
+            showRating
+            onFinishRating={this.ratingCompleted}
+            style={{
+              paddingVertical: 10,
+              backgroundColor: colors.lightGrey,
+
+            }}
+          />
+        </View>
+
       </View>
 
     )
   }
-
 }
-export default EvaluationPage
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    backgroundColor: colors.lightGrey,
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  evaluationContainer: {
+    paddingTop: 25,
+    alignItems: 'center',
+    paddingBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: colors.white,
+    flex: 1
+  },
+});
+
+const mapStateToProps = (state, ownProps) => {
+  const {classIndex, studentIndex} = ownProps.navigation.state.params;
+  state = state.data.teachers[0].classes[classIndex].students[studentIndex];
+  return state;
+};
+
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+  }, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(EvaluationPage);
