fbb12a4220c802c21a2ed562333b66716b732010
KhalidPM <45704194+KhalidPM@users.noreply.github.com>
2019-06-14 19:12:23 -0700

    Made textboxes on sign up screen more visible

--- Dev/QConnect/src/components/TopBanner.js
+++ Dev/QConnect/src/components/TopBanner.js
@@ -37,7 +37,7 @@ class TopBanner extends FontLoadingComponent {
                         </View>
 
                         <View style={styles.topRightView} >
-                            <TouchableOpacity style={{flex: 1, flexDirection: 'row',  backgroundColor: colors.green,  paddingRight: 20, height: 100, justifyContent: 'flex-end', alignItems: 'center'}}  onPress={() => { RightOnPress() }}>
+                            <TouchableOpacity style={{flex: 1, flexDirection: 'row', paddingRight: 20, height: 100, justifyContent: 'flex-end', alignItems: 'center'}}  onPress={() => { RightOnPress() }}>
                             <Icon
                                 name={RightIconName}
                                 type="font-awesome"  
