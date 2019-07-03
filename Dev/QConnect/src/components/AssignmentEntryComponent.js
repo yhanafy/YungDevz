86be80c9cdba125157b846b33d266c1b84444f06
awsm36z <awsmdud@gmail.com>
2019-07-02 20:46:43 -0700

    updated expo and began autocomplete

--- Dev/QConnect/src/components/AssignmentEntryComponent.js
+++ Dev/QConnect/src/components/AssignmentEntryComponent.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import {AutoComplete} from 'react-native-autocomplete-input';
-import { ScrollView, Text, TouchableOpacity } from 'react-native';
+// import React, { Component } from 'react';
+// import { AutoComplete } from 'react-native-autocomplete-input';
+// import { ScrollView, Text, TouchableOpacity, TextInput, View } from 'react-native';
 
-export default class AssignmentEntryComponent extends Component {
-    state = {
-        surahs: ["Al-Fatihah", "Al-Baqarah", "Aal-Imran"],
-        query: "Al-Fatihah"
-    }
-    render() {
-        return (
-            <ScrollView >
-                <Text>Hello</Text>
-                <AutoComplete
-                    data={this.state.surahs}
-                    defaultValue={this.state.query}
-                    onChangeText={text => this.setState({ query: text })}
-                    renderItem={({ item, i }) => (
-                        <TouchableOpacity onPress={() => this.setState({ query: item })}>
-                            <Text>{item}</Text>
-                        </TouchableOpacity>
-                    )} />
-            </ScrollView>
-        );
-    }
-}
+// export default class AssignmentEntryComponent extends Component {
+//     state = {
+//         surahs: ["Al-Fatihah", "Al-Baqarah", "Aal-Imran"],
+//         data = this.surahs,
+//         input = ""
+//     }
+
+//     addToState(word){
+//         this.state.input = word;
+//     }
+
+
+//     render() {
+//         const {query} = this.state.surahs;
+//         const data = query;
+//         return (
+//             <Autocomplete
+//             data={this.state.data}
+//             defaultValue={query}
+//             onChangeText={text => this.setState({ query: text })}
+//             renderItem={({ item, i }) => (
+//               <TouchableOpacity onPress={() => this.setState({ query: item })}>
+//                 <Text>{item}</Text>
+//               </TouchableOpacity>
+//             )}
+//           />
+//         );
+//     }
+// }
