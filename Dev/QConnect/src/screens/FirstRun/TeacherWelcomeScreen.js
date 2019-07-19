63f538aa35421394daec8f1c24ac88dd8ab5e7fe
zyadelgohary <zyadelgohary@users.noreply.github.com>
2019-07-18 23:14:16 -0700

    Update TeacherWelcomeScreen.js

--- Dev/QConnect/src/screens/FirstRun/TeacherWelcomeScreen.js
+++ Dev/QConnect/src/screens/FirstRun/TeacherWelcomeScreen.js
@@ -125,7 +125,11 @@ export class TeacherWelcomeScreen extends QcParentScreen {
   // In teacher welcome page, teacher ID will be passed as undefined, in which case
   // we will generate a new ID before saving to the store.
   saveProfileInfo = teacherID => {
-    const { name, phoneNumber, emailAddress, password } = this.state;
+    let { name, phoneNumber, emailAddress, password } = this.state;
+    name = name.trim();
+    phoneNumber = phoneNumber.trim();
+    emailAddress = emailAddress.trim();
+    password = password.trim();
 
     //Reset the confirmation dialog state cancelation state
     //In case user canceled the confirmation code dialog before, we reset that state so we can show the dialog again upon new submission
@@ -138,8 +142,8 @@ export class TeacherWelcomeScreen extends QcParentScreen {
 
     //generate a new id if this is a new teacher 
     if (teacherID === undefined) {
-      var nanoid = require('nanoid/non-secure')
-      teacherID = nanoid()
+      var nanoid = require('nanoid/non-secure');
+      teacherID = nanoid();
     }
 
     // save the relevant teacher properties
