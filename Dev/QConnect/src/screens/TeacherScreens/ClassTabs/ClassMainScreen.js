5df82db6dd77708324b9cbfcecd12681b65683b3
awsm36z <awsmdud@gmail.com>
2019-07-25 12:35:26 -0700

    working on loading screen

--- Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassMainScreen.js
+++ Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassMainScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { ScrollView, StyleSheet, FlatList } from "react-native";
+import { ScrollView, StyleSheet, FlatList, View, Text, TouchableOpacity, Image } from "react-native";
 import { connect } from "react-redux";
 import StudentCard from "components/StudentCard";
 import colors from "config/colors";
 import studentImages from "config/studentImages"
+import LoadingSpinner from '../../../components/LoadingSpinner';
 import { Font } from 'expo';
 import mapStateToCurrentClassProps from 'screens/TeacherScreens/helpers/mapStateToCurrentClassProps'
 import QcParentScreen from "screens/QcParentScreen";
+
 
 export class ClassMainScreen extends QcParentScreen {
 
@@ -18,11 +20,13 @@ export class ClassMainScreen extends QcParentScreen {
     // fact that we get an error about 'regular' font not loaded yet if we redirect to add class or edit class 
     // pages before explicitly loading the fonts. 
     // Todo: figure out a safer way to do this without having to hold the UI until the font is loaded.
-    await Font.loadAsync({
-      regular: require('assets/fonts/Montserrat-Regular.ttf'),
-      light: require('assets/fonts/Montserrat-Light.ttf'),
-      bold: require('assets/fonts/Montserrat-Bold.ttf'),
-    });
+
+    // await Font.loadAsync({
+    //   regular: require('assets/fonts/Montserrat-Regular.ttf'),
+    //   light: require('assets/fonts/Montserrat-Light.ttf'),
+    //   bold: require('assets/fonts/Montserrat-Bold.ttf'),
+    // });
+
 
     const { classId } = this.props;
 
@@ -31,32 +35,105 @@ export class ClassMainScreen extends QcParentScreen {
     }
   }
 
+  goToAddStudentPage() {
+    console.log("Hello");
+  };
+
   render() {
     const classId = this.props.classId;
+    if (this.state.fontLoaded === false) {
+      return (
+        <View style={styles.container}>
+          <LoadingSpinner isVisible={true} />
+        </View>
+      )
+    } else if (this.props.students.length === 0) {
+      /**
+       * ------Overview:
+       * The Page will display a message that will redirect the teacher to the 
+       * add student page if the class does not contain any students.
+       * 
+       * ------Components:
+       * We are using a touchable opacity with a large message telling the
+       * teacher that there are no students in the class, and a smaller message
+       * telling the teacher to click the text to add students.
+       * 
+       * ------Conditonal:
+       * The conditional will check to see if the length of the students array is 0,
+       * if it is, then there is no students in the class, and thus the class is empty,
+       * triggering the message. */
+      return (
+        <View
+          style={[styles.container, { alignItems: "center", justifyContent: "center", }]}>
+          {/**touchable opacity start */}
 
-    return (
-      <ScrollView style={styles.container}>
-        <FlatList
-          data={this.props.students}
-          keyExtractor={(item) => item.name} // fix, should be item.id (add id to classes)
-          renderItem={({ item }) => (
-            <StudentCard
-              key={item.id}
-              studentName={item.name}
-              background={colors.white}
-              profilePic={studentImages.images[item.imageId]}
-              currentAssignment={this.props.currentAssignments.byStudentId[item.id][0].name}
-              onPress={() =>
-                this.props.navigation.push("StudentProfile", {
-                  studentId: item.id,
-                  classId: classId
-                })
-              }
-            />
-          )}
-        />
-      </ScrollView>
-    );
+          <Image
+            source={require('assets/emptyClassGif.gif')}
+            style={{
+              width: 400,
+              height: 450,
+              resizeMode: 'contain'
+            }}
+          />
+          <TouchableOpacity
+            onPress={() => this.props.navigation.push("ClassEdit")}
+            style={{
+              flexDirection: "column",
+              alignItems: "center"
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 30,
+                color: colors.primaryDark,
+                flexDirection: "row",
+              }}
+            >
+              You Have no Students
+              </Text>
+
+            <Text
+              style={{
+                fontSize: 10,
+                color: colors.primaryDark,
+                flexDirection: "row",
+                justifyContent: "center"
+              }}>
+              Click me to add a student
+              </Text>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+
+
+    else {
+
+      return (
+        <ScrollView style={styles.container}>
+          <FlatList
+            data={this.props.students}
+            keyExtractor={(item) => item.name} // fix, should be item.id (add id to classes)
+            renderItem={({ item }) => (
+              <StudentCard
+                key={item.id}
+                studentName={item.name}
+                background={colors.white}
+                profilePic={studentImages.images[item.imageId]}
+                currentAssignment={this.props.currentAssignments.byStudentId[item.id][0].name}
+                onPress={() =>
+                  this.props.navigation.push("StudentProfile", {
+                    studentId: item.id,
+                    classId: classId
+                  })
+                }
+              />
+            )}
+          />
+        </ScrollView>
+      );
+    }
+
   }
 }
 
@@ -65,7 +142,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
     backgroundColor: colors.lightGrey,
-    flex: 1
+    flex: 3
   },
   classTitle: {
     color: colors.primaryDark,
