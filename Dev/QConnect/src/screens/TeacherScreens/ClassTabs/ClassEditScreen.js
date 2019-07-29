e7f59ff132606d0a674af55664a3dd00d2ecf42c
yhanafy <45704361+yhanafy@users.noreply.github.com>
2019-07-29 16:01:47 -0700

    Update ClassEditScreen.js

--- Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassEditScreen.js
+++ Dev/QConnect/src/screens/TeacherScreens/ClassTabs/ClassEditScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { ScrollView, View, StyleSheet, TextInput, FlatList, TouchableWithoutFeedback, Keyboard, Text, Alert } from "react-native";
+import { ScrollView, View, StyleSheet, TextInput, FlatList, TouchableWithoutFeedback, KeyboardAvoidingView, Keyboard, Text, Alert } from "react-native";
 import Toast, { DURATION } from 'react-native-easy-toast'
 import { connect } from "react-redux";
 import StudentCard from "components/StudentCard";
@@ -15,10 +15,136 @@ import { Icon } from 'react-native-elements';
 import strings from "config/strings";
 import mapStateToCurrentClassProps from "screens/TeacherScreens/helpers/mapStateToCurrentClassProps";
 import QcParentScreen from "screens/QcParentScreen";
+import classImages from "config/classImages";
+import { addClass } from "model/actions/addClass";
+import { saveTeacherInfo } from "model/actions/saveTeacherInfo";
+
 
 export class ClassEditScreen extends QcParentScreen {
+  //----------------------- state -------------------------------------
+  state = {
+    className: "",
+    classImageId: Math.floor(Math.random() * 10),
+    modalVisible: false,
+  };
 
+
+  // -------- event handlers, respond to user initiated events ----------
+  setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
+  onImageSelected(imageId) {
+    this.setState({ classImageId: imageId })
+    this.setModalVisible(false);
+  }
+
+  //---- helper function to determine if the entered class name is duplicate -------
+  classNameAlreadyExists() {
+    let { classes } = this.props;
+    for (let i = 0; i < classes.length; i++) {
+      if (classes[i].name.toLowerCase() === this.state.className.toLowerCase()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // saves the class into redux
+  addNewClass() {
+    let { classes } = this.props;
+
+    if (!this.state.className || this.state.className.trim().length === 0) {
+      Alert.alert(strings.Whoops, strings.PleaseMakeSureAllFieldsAreFilledOut);
+      return;
+    }
+
+    if (this.classNameAlreadyExists()) {
+      Alert.alert(strings.Whoops,
+        /*Message to say that it is an invalid input:*/
+        "Class Name already exists!",
+        [/*Button to exit */
+          { text: "OK" }
+        ]
+      )
+      return;
+    }
+
+    let newClassIndex = this.props.classes.length;
+
+    let classInfo = {
+      name: this.state.className,
+      imageId: this.state.classImageId,
+      students: []
+    };
+
+    this.props.addClass(classInfo);
+    this.props.saveTeacherInfo(
+      0, //todo: proper id here..
+      { currentClassIndex: newClassIndex }
+    );
+
+    //Navigates to the class
+    this.props.navigation.push("ClassEdit");
   name = "ClassEditScreen";
+    //----------------------- state -------------------------------------
+  state = {
+    className: "",
+    classImageId: Math.floor(Math.random() * 10),
+    modalVisible: false,
+  };
+
+
+
+  //---- helper function to determine if the entered class name is duplicate -------
+  classNameAlreadyExists(); {
+    let { classes } = this.props;
+    for (let i = 0; i < classes.length; i++) {
+      if (classes[i].name.toLowerCase() === this.state.className.toLowerCase()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // saves the class into redux
+  addNewClass(); {
+    let { classes } = this.props;
+
+    if (!this.state.className || this.state.className.trim().length === 0) {
+      Alert.alert(strings.Whoops, strings.PleaseMakeSureAllFieldsAreFilledOut);
+      return;
+    }
+
+    if (this.classNameAlreadyExists()) {
+      Alert.alert(strings.Whoops,
+        /*Message to say that it is an invalid input:*/
+        "Class Name already exists!",
+        [/*Button to exit */
+          { text: "OK" }
+        ]
+      )
+      return;
+    }
+
+    let newClassIndex = this.props.classes.length;
+
+    let classInfo = {
+      name: this.state.className,
+      imageId: this.state.classImageId,
+      students: []
+    };
+
+    this.props.addClass(classInfo);
+    this.props.saveTeacherInfo(
+      0, //todo: proper id here..
+      { currentClassIndex: newClassIndex }
+    );
+
+    //Navigates to the class
+    this.props.navigation.push("ClassEdit");
 
   // ---------- Helpers to initialize random suggested student images --------------
   //  2 gender neutral images, one female, and one male
@@ -64,7 +190,7 @@ export class ClassEditScreen extends QcParentScreen {
   }
 
   //Tests whether the entered input is already a student that exists in the given class
-  studentNameExists() {
+  studentNameExists(); {
     const students = this.props.students;
     const input = this.state.newStudentName.trim();
 
@@ -78,7 +204,7 @@ export class ClassEditScreen extends QcParentScreen {
   }
 
   // ----------- Redux function to persist the added student ------------------------
-  addNewStudent(classIndex) {
+  addNewStudent(classIndex); {
     if (!this.state.newStudentName) {
       Alert.alert(strings.Whoops, strings.PleaseInputAName);
     } else if (this.studentNameExists()) {
@@ -114,7 +240,7 @@ export class ClassEditScreen extends QcParentScreen {
   }
 
   // ------- event handlers of when images are selected or being selected ---------
-  onImageSelected(index) {
+  onImageSelected(index); {
     let candidateImages = this.state.highlightedImagesIndices;
 
     if (!this.state.highlightedImagesIndices.includes(index)) {
@@ -130,13 +256,13 @@ export class ClassEditScreen extends QcParentScreen {
     this.setModalVisible(false);
   }
 
-  setModalVisible(visible) {
+  setModalVisible(visible); {
     this.setState({ modalVisible: visible });
   }
 
   // ------- Render method: Main entry to render the screen's UI ------------------
 
-  render() {
+  render(); {
     const { deleteStudent, addStudent, classIndex, students } = this.props;
 
     if (this.state.highlightedImagesIndices.length == 0) {
@@ -145,6 +271,51 @@ export class ClassEditScreen extends QcParentScreen {
 
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <View
+          style={styles.container}
+        >
+
+          <ImageSelectionModal
+            visible={this.state.modalVisible}
+            images={classImages.images}
+            cancelText={strings.Cancel}
+            setModalVisible={this.setModalVisible.bind(this)}
+            onImageSelected={this.onImageSelected.bind(this)}
+            screen={this.name}
+          />
+
+          <View style={styles.picContainer}>
+            <Image
+              style={styles.profilePic}
+              source={classImages.images[this.state.classImageId]}
+              ResizeMode="contain" />
+            <TouchableText
+              text={strings.EditClassImage}
+              onPress={() => this.setModalVisible(true)} />
+          </View>
+
+          <View style={styles.bottomContainer}>
+            <TextInput
+              style={styles.textInputStyle}
+              placeholder={strings.WriteClassNameHere}
+              onChangeText={classInput =>
+                this.setState({
+                  className: classInput
+                })
+              }
+            />
+
+            <QcActionButton
+              text={strings.AddClass}
+              onPress={() => {
+                this.addNewClass();
+              }}
+              screen={this.name}
+            />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
         <ScrollView style={styles.container}>
           <ImageSelectionModal
             visible={this.state.modalVisible}
